0f045182a6dee160a675e1bbc6c4bfc4e1f3fd71
Chen Wang <wangvsa@163.com>
2018-02-15 09:17:35 -0600

    Draw non AMR data

--- plots/flash_plot.py
+++ plots/flash_plot.py
@@ -9,20 +9,40 @@ import matplotlib.animation as animation
 # Read from a hdf5 file to a numpy array
 def hdf5_to_numpy(filename, var_name="dens"):
     f = h5py.File(filename, 'r')
+    print f.keys()
     data = f[var_name][:]
     return data
 
+# Stack a shape of (N, 1, ny, nx) to shape (ny, N*nx)
+def stack(data):
+    data = np.vstack(data)
+    data = np.hstack(data)
+    return data
+
+
 def show_heatmap(filename):
-    data = hdf5_to_numpy(filename)[100][0]
-    print data.shape
-    print data
+    data = hdf5_to_numpy(filename)
+    data = stack(data)
     plt.imshow(data, cmap='hot', interpolation='nearest')
     plt.show()
 
 
+def test(filename):
+    data = hdf5_to_numpy(filename, "dens")
+    print "dens:", data.shape, data
+    data = stack(data)
+    print "dens:", data.shape, data
+
+    data = hdf5_to_numpy(filename, "coordinates")
+    print "coordinates:", data.shape, data
+
+    data = hdf5_to_numpy(filename, "processor number")
+    print "processor number:", data.shape, data
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 :
-        sys.exit("Usage: python plot dataset_directory")
+        sys.exit("Usage: python flash_plot.py hdf5_plt_file_path")
     path = sys.argv[1]
     show_heatmap(path)
+    #test(path)
