1d4cda05e6442c44d0cf9e65f4019559b04685ff
Chen Wang <wangvsa@163.com>
2018-02-27 14:05:43 -0600

    Use the function implemented in flash_par.py

--- detector/FlashDetector/flash_restart.py
+++ detector/FlashDetector/flash_restart.py
@@ -1,88 +1,83 @@
 '''
 Generate flash.par to restart from a corrupted checkpoint
 '''
-import commands
 import h5py
 import random
 import glob
 import os
 from bits import bit_flip
 import math
+import flash_par
 
-SOD_DIR = "/home/wangchen/Sources/FLASH4.4/objects/Sod_multi/"
 FLASH_APPS = ["Blast2", "BlastBS", "BrioWu", "Cellular",
                 "DMReflection", "RHD_Sod", "Sedov", "Sod"]
 
-par_file = SOD_DIR + "flash.par"
-
-# delete last two lines
-def delete_last_lines():
-    commands.getstatusoutput("head -n -2 " + par_file + " > tmp.par")
-    commands.getstatusoutput("mv tmp.par " + par_file)
-
-def append_to_par(restart_point):
-    # Specify restart point
-    commands.getstatusoutput("echo checkpointFileNumber = " + str(restart_point) + " >> " + par_file)
-    # Specify end point
-    commands.getstatusoutput("echo nend = " + str(restart_point+10) + " >> " + par_file)
+BASE_DIR = "/home/wangchen/tmp/data_0/"
+par_file = BASE_DIR + "flash.par"
 
 
+'''
+Copy par file
+Modify it so we can restart from the given corrupted checkpoint
+Also set the nend to 10 timesteps after restarting
+'''
+def modify_par_file(basenm, restart_point):
+    pars = flash_par.read_par_file(par_file)
+    pars['basenm'] = "\""+basenm+"\""
+    pars['nend'] = str(restart_point+10)
+    pars['checkpointFileNumber'] = str(restart_point)
+    pars['restart'] = ".true."
+    new_par_file = par_file + "." + basenm[:-1] # remove the last _
+    flash_par.write_par_file(new_par_file, pars)
+    return new_par_file
+
+
+'''
+Copy a clean checkpoint
+Insert an error into the copied file
+Return the basenm which contains the error info: restart_point,x,y,bit
+'''
 def insert_error(restart_point):
+    x = random.randint(0, 1000-1)
+    y = random.randint(0, 1000-1)
+    bit = random.randint(0, 10-1)
+
+
     filenumber = ("0000"+str(restart_point))[-4:]
-    checkpoint_file = SOD_DIR + "clean_checkpoints/sod_hdf5_chk_"+filenumber
-    commands.getstatusoutput("cp "+checkpoint_file+" "+SOD_DIR)
-    checkpoint_file = SOD_DIR + "sod_hdf5_chk_"+ filenumber
-    f = h5py.File(checkpoint_file, 'r+')
+    basenm = "error_%s_%s_%s_%s_" %(restart_point, x, y, bit)
+    clean_checkpoint_file = BASE_DIR + "hdf5_chk_"+filenumber
+    corrupted_checkpoint_file = BASE_DIR+basenm+"hdf5_chk_"+filenumber
 
-    blockId = random.randint(0, f['dens'].shape[0]-1)
-    x = random.randint(0, 7)
-    y = random.randint(0, 7)
-    org = f['dens'][blockId, 0, y, x]
+    # Copy a corrupted checkpoint file
+    os.system("cp "+clean_checkpoint_file+" "+corrupted_checkpoint_file)
+    f = h5py.File(corrupted_checkpoint_file, 'r+')
 
-    bit = random.randint(1, 10)
+    # Insert error
+    org = f['dens'][0, 0, y, x]
     error = bit_flip(org, bit)
 
     if math.isnan(error) or math.isinf(error):
-        error = org * 5
-    if error > org * 5:
-        error = org * 5
-    f['dens'][blockId, 0, y, x] = error
-    f['dens'][blockId, 0, y, x] = 5*org
-    coordinate = list(f['coordinates'][blockId])
+        error = org * 1000
+    if error > org * 1000: error = 1000*org
+    f['dens'][0, 0, y, x] = error
     f.close()
 
-    basename = "sod_"+str([restart_point, blockId, x, y])+"_"+str(coordinate)+"_"
-    return basename
-
-
-def rename_plot_files(basename):
-    for filename in glob.iglob(SOD_DIR+"sod_hdf5_plt_cnt*"):
-        newname = SOD_DIR + basename + filename.split("/")[-1]
-        os.rename(filename, newname)
-
+    return basenm
 
 def test():
     # Restart from checkpoint i
-    for restart_point in range(1000):
-        if restart_point == 100:
-        #if (restart_point-15) % 50 == 0:
-            # Insert error
+    for restart_point in range(10):
+        if restart_point == 5:
+
             basename = insert_error(restart_point)
 
-            # Modify par file to let it restart from given checkpoint
-            delete_last_lines()
-            append_to_par(restart_point)
+            new_par_file = modify_par_file(basename, restart_point)
 
             # Restart the program
-            cmd = "cd "+ SOD_DIR +" && mpirun -np 8 ./flash4"
-            status = commands.getstatusoutput(cmd)[0]
-            rename_plot_files(basename)
+            cmd = "cd "+ BASE_DIR +" && mpirun -np 8 ./flash4 -par_file "+new_par_file
+            os.system(cmd)
 
             # CD back
-            commands.getstatusoutput("cd /home/wangchen/Sources/aletheia/detector/FlashDetector/")
+            os.system("cd /home/wangchen/Sources/aletheia/detector/FlashDetector/")
 
-            print "restart at:", restart_point, ", status:", status
-            print basename
-
-for i in range(1000):
-    test()
+test()
