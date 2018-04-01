c662123a7eee85b6ecae2c694cb704b50491180c
Chen Wang <wangvsa@163.com>
2018-04-01 18:21:57 -0500

    Delete old files about heat_distribution

--- detector/heat_distribution_multi_iterations/heat_classifier.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#PBS -N HEAT_CLASSIFIER
-#PBS -l nodes=1:ppn=16
-#PBS -l walltime=01:59:00
-#PBS -j oe
-
-cd /home/wangchen/detector/heat_distribution_multi_iterations
-python classifier.py
