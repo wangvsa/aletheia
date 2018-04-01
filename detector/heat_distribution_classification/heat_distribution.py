b27e4b431718effda627039a3f156e4281325d9d
Chen Wang <wangvsa@163.com>
2018-04-01 18:21:26 -0500

    Modifty heat_distribution to dump data

--- detector/heat_distribution_classification/heat_distribution.py
+++ detector/heat_distribution_classification/heat_distribution.py
@@ -2,10 +2,17 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from skimage.util.shape import view_as_windows, view_as_blocks
+
 
 T = 200                     # total number of iterations
 ROWS = 480                   # rows of the plate
 COLUMNS= 480                 # columns of the plate
+
+WINDOW_ROWS = 60
+WINDOW_COLS = 60
+WINDOW_OVERLAP = 20
+
 HEATING_DEVICE_SIZE = 3     # the length of the heating source
 
 # heating device shaped like X
@@ -14,25 +21,56 @@ Gr = np.eye(HEATING_DEVICE_SIZE)
 for iGr in range(HEATING_DEVICE_SIZE):
     Gr[iGr,-iGr-1] = 1
 
+# Insert heating device in the center
 # Function to set M values corresponding to non-zero Gr values
-def assert_heaters(M, Gr):
+def assert_heaters(frame, Gr):
     start_row, end_row = (ROWS-HEATING_DEVICE_SIZE)/2, (ROWS+HEATING_DEVICE_SIZE)/2
     start_column , end_column = (COLUMNS-HEATING_DEVICE_SIZE)/2, (COLUMNS+HEATING_DEVICE_SIZE)/2
-    M[start_row:end_row, start_column:end_column] = np.where(Gr > 0, Gr, M[start_row:end_row, start_column:end_column])
+    frame[start_row:end_row, start_column:end_column] = np.where(Gr > 0, Gr, frame[start_row:end_row, start_column:end_column])
+
+
+def split_to_windows(frame, rows, cols, overlap):
+    step = cols - overlap
+    windows = view_as_windows(frame, (rows, cols), step=step)
+    return np.vstack(windows)
+
+def split_to_blocks(frame, rows, cols):
+    blocks = view_as_blocks(frame, (rows, cols))
+    return np.vstack(blcosk)
+
+
+# Split a frame to windows/blocks and save to a npy file
+def dump_data_file(frame, filename):
+    windows = split_to_windows(frame, WINDOW_ROWS, WINDOW_COLS, WINDOW_OVERLAP)
+    np.save(filename, windows)
 
 
 # Build MM, a list of matrices, each element corresponding to M at a given step
-def heat_distribution():
-    MM = np.zeros((T, ROWS, COLUMNS))
-    assert_heaters(MM[0], Gr)
-    for i in range(1, T):
-        MM[i] = MM[i-1].copy()
+def heat_distribution(interval = 1):
+    frame = np.zeros((ROWS, COLUMNS))
+    assert_heaters(frame, Gr)
+    last_frame = np.copy(frame)
+    for i in range(0, T):
+
+        print("iter: %s" %(i))
+
+        # Stencil computation
         for x in range(1, ROWS-1):
             for y in range(1, COLUMNS-1):
-                MM[i,x,y] = (MM[i,x-1,y] + MM[i,x+1,y] + MM[i,x,y-1] + MM[i,x,y+1])/4
+                frame[x,y] = (last_frame[x-1,y] + last_frame[x+1,y] +
+                                last_frame[x,y-1] + last_frame[x,y+1])/4.0
+        # Dump to file
+        if i % interval == 0:
+            filename = str(i)+".npy"
+            print("save to file: %s" %(filename))
+            dump_data_file(frame, filename)
+
         # Re-assert heaters
-        assert_heaters(MM[i], Gr)
-    return MM
+        assert_heaters(frame, Gr)
+
+if __name__ == "__main__":
+    heat_distribution(interval = 20)
+
 
 def insert_error(MM):
     has_error = np.zeros(len(MM))    # if has error
