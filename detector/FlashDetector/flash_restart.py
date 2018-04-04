e641b9c719af8c9ce5faff8cdda6597014105f9c
Chen Wang <wangvsa@163.com>
2018-04-04 17:08:01 -0500

    Now can inject single error at one frame

--- detector/FlashDetector/flash_restart.py
+++ detector/FlashDetector/flash_restart.py
@@ -17,12 +17,12 @@ FLASH_APPS = ["Blast2", "BlastBS", "BrioWu", "Cellular",
 
 def get_flip_error(val):
     while True:
-        pos = random.randint(0, 20)
+        pos = random.randint(1, 20)
         error =  bit_flip(val, pos)
         if not math.isnan(error) and not math.isinf(error):
             break
-    #error = min(10e+3, error)
-    #error = max(-10e+3, error)
+    error = min(10e+3, error)
+    error = max(-10e+3, error)
     #if error > 0 and error < 1e-5: error = 0
     return error
 
@@ -36,7 +36,7 @@ def modify_par_file(data_dir, basenm, restart_point):
 
     pars = flash_par.read_par_file(par_file)
     pars['basenm'] = "\""+basenm+"\""
-    pars['nend'] = str(restart_point+4)
+    pars['nend'] = str(restart_point+99)
     pars['checkpointFileNumber'] = str(restart_point)
     pars['restart'] = ".true."
 
@@ -45,7 +45,7 @@ def modify_par_file(data_dir, basenm, restart_point):
     pars['checkpointFileIntervalStep'] = "0"
     pars['plotfileIntervalStep'] = "1"
 
-    new_par_file = par_file + "." + basenm[:-1] + ".new_par" # remove the last _, "new_par" just easy for my pbs script to handle
+    new_par_file = data_dir + "/flash." + basenm + ".new_par" # remove the last _, "new_par" just easy for my pbs script to handle
     flash_par.write_par_file(new_par_file, pars)
     return new_par_file
 
@@ -59,7 +59,8 @@ def insert_errors(data_dir, restart_point):
 
     filenumber = ("0000"+str(restart_point))[-4:]
     basenm = "error_%s_" %(restart_point) + str(int(time.time()*1000))
-    clean_checkpoint_file = data_dir + "sod_hdf5_chk_"+filenumber
+    clean_data_dir =  "_".join(data_dir.split("_")[0:2]) + "/" #e.g. data_11_0to20 -> data_11
+    clean_checkpoint_file = clean_data_dir + "sod_hdf5_chk_"+filenumber
     corrupted_checkpoint_file = data_dir + basenm + "hdf5_chk_" + filenumber
 
     # Copy a corrupted checkpoint file
@@ -67,7 +68,7 @@ def insert_errors(data_dir, restart_point):
     f = h5py.File(corrupted_checkpoint_file, 'r+')
     f_org = f['dens'][:].copy()
 
-    # Inset one into each of the windows of a 480x480 frame,
+    # Insert one into each of the windows of a 480x480 frame,
     # so we will have 11x11 windows (60, 60, overlap:20)
     valid, all = 0, 0
     for start_y in range(20, 460, 40):
@@ -75,28 +76,42 @@ def insert_errors(data_dir, restart_point):
             x = start_x + random.randint(0, 20)
             y = start_y + random.randint(0, 20)
             # Insert error
-            org = f['dens'][0, 0, y, x]
-            error = get_flip_error(org)
-
-            threshold = (np.max(f_org) - np.min(f_org)) * 0.00078125
-            if abs(error - org) > threshold: valid = valid + 1
-            all = all+1
-
-            f['dens'][0, 0, y, x] = error
+            f['dens'][0, 0, y, x] = get_flip_error(f['dens'][0,0,y,x])
 
     f.close()
-    print "valid:", valid, ", all:", all
-    return basenm, valid, all
+    return basenm
+
+# Only insert one error into one checkpoint
+def insert_error(data_dir, restart_point):
+
+    win_x, win_y = random.randint(0, 11-1), random.randint(0, 11-1)
+    x = np.arange(20, 460, 40)[win_x] + random.randint(0, 20)
+    y = np.arange(20, 460, 40)[win_y] + random.randint(0, 20)
+    error_win_id = win_x * 11 + win_y
+
+    filenumber = ("0000"+str(restart_point))[-4:]
+    basenm = "error_%s_%s_%s_%s" %(restart_point, error_win_id, x, y)
+
+    clean_data_dir =  data_dir + "/clean/"
+    clean_checkpoint_file = clean_data_dir + "sod_hdf5_chk_"+filenumber
+
+    corrupted_checkpoint_file = data_dir + basenm + "hdf5_chk_" + filenumber
+
+    # Copy a corrupted checkpoint file
+    os.system("cp "+clean_checkpoint_file+" "+corrupted_checkpoint_file)
+    f = h5py.File(corrupted_checkpoint_file, 'r+')
+    print("old:", f['dens'][0,0,x,y])
+    f['dens'][0, 0,x,y] = get_flip_error(f['dens'][0,0,x,y])
+    print("new:", f['dens'][0,0,x,y])
+
+    f.close()
+    return basenm
 
 def restart(data_dir):
-    valid, all = 0, 0
-    for restart_point in range(0, 200, 20): # 0~200, step=20
-        basename, t1, t2 = insert_errors(data_dir, restart_point)
-        valid = valid + t1
-        all = all + t2
+    for restart_point in range(50, 100, 10): # 0~200, step=20
+        basename = insert_error(data_dir, restart_point)
         new_par_file = modify_par_file(data_dir, basename, restart_point)
         print(new_par_file)
-    print valid, all
 
 
 # Usage python flash_restart.sh data_dir
